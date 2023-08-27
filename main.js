0886d96e3ec47088647aa91003692ef2100b3caa
Daffi R. D <116930031+R4WLYX@users.noreply.github.com>
2023-08-27 14:34:41 +0700

    I hate CSS

--- main.js
+++ main.js
@@ -1,34 +1,88 @@
 const video = document.querySelector(".fullscreen-video");
+const canvas = document.querySelector("#canvas");
 const cover = document.querySelector(".cover");
 const title = document.querySelector(".menu-title");
 const liveInfo = document.querySelector(".live-info");
 const playInfo = document.querySelector(".play-info");
 const counter = document.querySelector(".counter");
 const congrtas = document.querySelector(".bro-won");
+const replayInfo = document.querySelector(".replay-info");
 const liveCounter = document.querySelector(".lives");
-let mouseX = 0;
-let mouseY = 0;
+
+let ctx = canvas.getContext("2d");
+let mouseX, mouseY;
+let videoDrawWidth, videoDrawHeight, videoX, videoY;
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function start() {
-    document.addEventListener('keydown', function(event) {
-        if (event.keyCode == 32) {
-            document.removeEventListener('keydown', playVideo);
-            playVideo();
-        }
+    updateDimensions();
+
+    video.crossOrigin = "anonymous";
+
+    ctx.drawImage(video, videoX, videoY, videoDrawWidth, videoDrawHeight);
+
+    document.addEventListener("keydown", function handler(event) {
+        if (event.keyCode != 32) { return; }
+        event.currentTarget.removeEventListener("keydown", handler);
+        playVideo();
     });
-    document.addEventListener('mousemove', function(event) {
+
+    document.addEventListener("mousemove", function (event) {
         mouseX = event.clientX;
         mouseY = event.clientY;
     });
-    video.addEventListener('ended', function(event) {
-        video.style.display = "none";
-        congrtas.style.display = "block";
-        liveCounter.style.display = "none";
+
+    video.addEventListener("play", function(event) {
+        mainLoop();
     });
+
+    video.addEventListener("ended", function(event) {
+        cover.style.display = "block";
+        congrtas.style.display = "block";
+        replayInfo.style.display = "block";
+        liveCounter.style.display = "none";
+
+        document.addEventListener("keydown", function handler(event) {
+            if (event.keyCode != 32) { return; }
+            event.currentTarget.removeEventListener("keydown", handler);
+
+            video.currentTime = 0;
+            video.pause();
+
+            resetStyles();
+            playVideo();
+        });
+    });
+}
+
+function updateDimensions() {
+    const videoWidth = video.videoWidth;
+    const videoHeight = video.videoHeight;
+    
+    const canvasAspect = canvas.width / canvas.height;
+    const videoAspect = videoWidth / videoHeight;
+
+    if (videoAspect > canvasAspect) {
+        videoDrawWidth = canvas.width;
+        videoDrawHeight = canvas.width / videoAspect;
+        videoX = 0;
+        videoY = (canvas.height - videoDrawHeight) / 2;
+    } else {
+        videoDrawHeight = canvas.height;
+        videoDrawWidth = canvas.height * videoAspect;
+        videoX = (canvas.width - videoDrawWidth) / 2;
+        videoY = 0;
+    }
+}
+
+function resetStyles() {
+    counter.style.display = "none";
+    congrtas.style.display = "none";
+    replayInfo.style.display = "none";
+    liveCounter.style.display = "none";
 }
 
 async function playVideo() {
@@ -36,19 +90,31 @@ async function playVideo() {
     liveInfo.style.display = "none";
     playInfo.style.display = "none";
     counter.style.display = "block";
-    liveCounter.style.display = "block";
-
+    
     for (let i = 3; i >= 1; i--) {
         counter.innerHTML = i;
         await sleep(1000);
     }
-
+    
+    liveCounter.style.display = "block";
     cover.style.display = "none";
     counter.style.display = "none";
+    
     video.play();
-    mainLoop();
 }
 
 function mainLoop() {
+    if (video.paused || video.ended) { return; }
+    updateDimensions();
+    ctx.drawImage(video, videoX, videoY, videoDrawWidth, videoDrawHeight);
+
+    const pixel = ctx.getImageData(mouseX, mouseY, 1, 1).data;
+
+    const red = pixel[0];
+    const green = pixel[1];
+    const blue = pixel[2];
+
+    console.log("r:", red, "g:", green, "b:", blue);
+
     requestAnimationFrame(mainLoop);
-}
+};
